96d85a2d284705806ce407941b4818846d98858f
Aryan Chaurasia <31214277+aryan1107@users.noreply.github.com>
2021-02-01 20:37:18 +0530

    Add files via upload
    Pset 5 Upload

---
+++ pset5/speller/dictionary.c
@@ -0,0 +1,69 @@
+// Implements a dictionary's functionality
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+#include "dictionary.h"
+
+// Represents a node in a hash table
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+}
+node;
+
+// Number of buckets in hash table
+const unsigned int N = 1088;
+
+// Hash table
+node *table[N];
+
+// Returns true if word is in dictionary, else false
+bool check(const char *word)
+{
+    // TODO
+    return false;
+}
+
+// Hashes word to a number
+unsigned int hash(const char *word)
+{
+    // TODO
+    return 0;
+}
+
+// Loads dictionary into memory, returning true if successful, else false
+bool load(const char *dictionary)
+{
+    // Open file
+    FILE *file = fopen(dictionary, "r");
+
+    if (!file)
+    {
+        return 1;
+    }
+
+    while (fread(*dictionary, sizeof(dictionary), 1, file) != EOF)
+    {
+
+    }
+    return false;
+}
+
+// Returns number of words in dictionary if loaded, else 0 if not yet loaded
+unsigned int size(void)
+{
+    // TODO
+    return 0;
+}
+
+// Unloads dictionary from memory, returning true if successful, else false
+bool unload(void)
+{
+    // TODO
+    return false;
+}
